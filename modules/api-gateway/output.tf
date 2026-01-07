fb778b5458b82210255cc950a4f9965c3c0238a4
Luan Pessoa <devluan.pessoa@gmail.com>
2026-01-06 23:36:23 -0300

    feat(api-gateway): refactor API routes and integrations
    - Introduced a new locals.tf file to define API routes and their configurations.
    - Replaced individual resource and method definitions with dynamic generation based on the defined routes.
    - Updated the main.tf to utilize the new route definitions for creating API Gateway resources and methods.
    - Simplified the integration setup for API methods by leveraging the new structure.
    - Modified output.tf to correctly reference the invoke URL of the API Gateway deployment.

--- modules/api-gateway/output.tf
+++ modules/api-gateway/output.tf
@@ -1,6 +1,6 @@
 output "invoke_url" {
   description = "Invoke URL of the API Gateway"
-  value       = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.dev.stage_name}"
+  value       = aws_api_gateway_deployment.this.invoke_url
 }
 
 # Debug outputs
